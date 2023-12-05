fad9e5d0681227d609ac99f1d42b88dd4c142566
Alex <131786787+Spelks@users.noreply.github.com>
2023-12-05 09:47:24 +0000

    create if statement to check tag array. Correct inputValue variable to accept numbers.

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const htmlAnswers = document.querySelector(".html-answers");
 
 searchInput.addEventListener("keypress", (e)=> {
     if (e.key === "Enter") {
-        htmlAnswers.innerText += `${searchInput.value.replace(/[^a-z, A-Z]/g, "")}`.toLocaleLowerCase() + "\n";
+        const inputValue = searchInput.value.toLowerCase().replace(/[^a-z1-6]/g, "");
+        if(htmlTags.includes(inputValue)) {
+            guessedTags.push(inputValue);
+            htmlAnswers.innerText += inputValue + "\n";
+        }
         searchInput.value = "";
     }
 });
