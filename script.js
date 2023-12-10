02d560271760483d0478a712b5027cb0477bd72d
Alex <131786787+Spelks@users.noreply.github.com>
2023-12-10 10:45:33 +0000

    Implement dropdown functionality for 'file', 'view' and 'help'.

--- script.js
+++ script.js
@@ -6,6 +6,73 @@ const explorerBar = document.querySelector(".explorer-container");
 const memoryTest = document.querySelector(".memory-test");
 const indexHTML = document.querySelector(".index-html");
 const memoryTestIcon = document.querySelector(".memory-test i.fa");
+const fileMenu = document.querySelector(".file-menu");
+const fileItem = document.querySelector(".file-item");
+const viewMenu = document.querySelector(".view-menu");
+const viewItem = document.querySelector(".view-item");
+const helpMenu = document.querySelector(".help-menu");
+const helpItem = document.querySelector(".help-item");
+
+helpItem.addEventListener("click", ()=> {
+    htmlTags.forEach(tag => {
+        answerTag.innerText += `${"<"}${tag}${">"}` + "\n";
+    })  
+})
+
+//Event listeners to show clicked dropdown and hide other open ones
+fileMenu.addEventListener("click", ()=> {
+    fileItem.classList.toggle("show-file-item");
+    viewItem.classList.remove("show-file-item");
+    helpItem.classList.remove("show-file-item");
+})
+viewMenu.addEventListener("click", ()=> {
+    viewItem.classList.toggle("show-file-item");
+    fileItem.classList.remove("show-file-item");
+    helpItem.classList.remove("show-file-item");
+})
+helpMenu.addEventListener("click", ()=> {
+    helpItem.classList.toggle("show-file-item");
+    fileItem.classList.remove("show-file-item");
+    viewItem.classList.remove("show-file-item");
+})
+
+//close any pop-ups when clicking outside of them
+document.addEventListener("click", (event)=> {
+    const isDropdownActive = event.target.closest(".file-menu, .file-iten, .view-menu, .view-item, .help-menu, .help-item");
+    if(!isDropdownActive) {
+        fileItem.classList.remove("show-file-item");
+        viewItem.classList.remove("show-file-item");
+        helpItem.classList.remove("show-file-item");        
+    }
+})
+
+//refreshes window
+fileItem.addEventListener("click", ()=> {
+    window.location.reload();
+})
+
+//Full screen toggle
+viewItem.addEventListener("click", toggleFullScreen);
+
+function toggleFullScreen() {
+    if ((document.fullScreenElement && document.fullScreenElement !== null) || (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+       if (document.documentElement.requestFullScreen) {
+          document.documentElement.requestFullScreen();
+       } else if (document.documentElement.mozRequestFullScreen) {
+          document.documentElement.mozRequestFullScreen();
+       } else if (document.documentElement.webkitRequestFullScreen) {
+          document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+       }
+    } else {
+       if (document.cancelFullScreen) {
+          document.cancelFullScreen();
+       } else if (document.mozCancelFullScreen) {
+          document.mozCancelFullScreen();
+       } else if (document.webkitCancelFullScreen) {
+          document.webkitCancelFullScreen();
+       }
+    }
+ }
 
 const guessedTags = [];
 
